2072deafb78688fbec7b50e005b4e461112c264b
Fernando Garrido <fdo.grafica@gmail.com>
2021-11-21 10:54:52 -0300

    move

--- utils/alowedHosts.js
+++ utils/alowedHosts.js
@@ -1,6 +1,6 @@
 // origenes autorizados
 exports.AuthHosts = (req, res, next) => {
-  const hosts = ["localhost:5252", "http://localhost"];
+  const hosts = [];
   if (hosts.indexOf(req.headers.host) >= 0) return next();
   return res.sendStatus(403);
 };
